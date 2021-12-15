f8e8684895c44fbfaf476bdc2b10aaf3186d0da5
PLUTO.21 <1603613240@qq.com>
2021-12-16 01:08:26 +0800

    搭建项目配置环境

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      extensions: [],
+      alias: {
+        // '@':'src' 内部已经配置过这个别名
+        'assets': '@/assets',
+        'views': '@/views'
+      }
+    }
+  }
+}
