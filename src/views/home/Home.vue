9a01bdbccfe3f3aec9355c02a5a4b6129e84427b
PLUTO.21 <1603613240@qq.com>
2021-12-18 14:04:30 +0800

    完成首页的推荐模块

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div id="home">
-    <h2>首页</h2>
+    <nav-bar class="home-nav">
+      <div slot="center">心灵商铺</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend-view :recommends="recommends"></home-recommend-view>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar.vue'
+import HomeSwiper from './childComps/HomeSwiper.vue'
+import HomeRecommendView from './childComps/HomeRecommendView.vue'
+
+import { getHomeMultidata } from '@/network/home.js'
+
 export default {
   name: 'Home',
-  components: {},
+  components: { NavBar, HomeSwiper, HomeRecommendView },
   data() {
-    return {}
+    return {
+      // result: null
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      // console.log(res)
+      // this.result = res
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
   }
 }
 </script>
 
 <style scoped>
+.home-nav {
+  background-color: rgb(100, 200, 300);
+  color: #fff;
+}
 </style>
